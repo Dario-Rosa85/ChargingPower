7b8090ea883527267017014c532a490ea924ba9e
Dario Rosa <dario.rosa85@gmail.com>
2024-10-28 17:26:26 -0300

    working example done and tested. ready to sync

--- src/building_operators/SYK_hamiltonian.jl
+++ src/building_operators/SYK_hamiltonian.jl
@@ -1,9 +1,9 @@
 #This function builds the quadratic SYK Hamiltonian based on a given graph
 function SYK_hamiltonian(majorana_operators, graph)
     n_majorana = length(majorana_operators)
-    SYK_hamiltonian_matrix = spzeros(2^(n_majorana/2), 2^(n_majorana/2))
+    SYK_hamiltonian_matrix = spzeros(ComplexF64,2^floor(Int, n_majorana/2), 2^floor(Int, n_majorana/2))
     for edge in edges(graph) 
-        SYK_hamiltonian_matrix .+= (im/2) * sqrt(n_majorana/(2 * length(edges))) * randn().* majorana_operators[src(edge)] * majorana_operators[dst(edge)]
+        SYK_hamiltonian_matrix .+= (im/2) * sqrt(n_majorana/(2 * length(edges(graph)))) * randn().* majorana_operators[src(edge)] * majorana_operators[dst(edge)]
     end
     return SYK_hamiltonian_matrix
 end
--- src/building_operators/battery_hamiltonians.jl
+++ src/building_operators/battery_hamiltonians.jl
@@ -1,9 +1,9 @@
 # This function creates the Hamiltonian for the Z-model
 function Z_model_hamiltonian(majorana_operators)
     n_majorana = length(majorana_operators)
-    hamiltonian_matrix = spzeros(2^(n_majorana/2), 2^(n_majorana/2))
-    for i in 1:Int(floor(n_majorana/2)) 
-        hamiltonian_matrix .+= -2 * im .* majorana_operators[2 * i - 1] * majorana_operators[2 * i] 
+    hamiltonian_matrix = spzeros(ComplexF64,2^floor(Int, n_majorana/2), 2^floor(Int, n_majorana/2))
+    for i in 1:floor(Int, n_majorana/2) 
+        hamiltonian_matrix .+= -1 * im .* majorana_operators[2 * i - 1] * majorana_operators[2 * i] 
     end
     return hamiltonian_matrix
 end
@@ -11,14 +11,14 @@ end
 # This function creates the Hamiltonian for the X-model
 function X_model_hamiltonian(majorana_operators)
     n_majorana = length(majorana_operators)
-    hamiltonian_matrix = spzeros(2^(n_majorana/2), 2^(n_majorana/2))
-    for k in 1: Int(floor(n_majorana/2)) 
+    hamiltonian_matrix = spzeros(ComplexF64, 2^floor(Int, n_majorana/2), 2^floor(Int, n_majorana/2))
+    for k in 1: floor(Int, n_majorana/2) 
         if k == 1
-            hamiltonian_matrix .+= sqrt(2) .* majorana_operators[1]
+            hamiltonian_matrix .+= (1/sqrt(2)) .* majorana_operators[1]
         else
-            provisional_matrix = majorana_operators[1]
+            provisional_matrix = sqrt(2) .* majorana_operators[1]
             for j in 2:(2 * k - 1) 
-                provisional_matrix *= majorana_operators[j]
+                provisional_matrix *= ((1/sqrt(2)) .* majorana_operators[j])
             end
             hamiltonian_matrix .+= 2^(k - 1/2) * (-1)^(k + 1) .* provisional_matrix
         end
