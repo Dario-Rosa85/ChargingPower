9eb459df867b93d86b3fb7f64619a764f66baea0
Dario Rosa <dario.rosa85@gmail.com>
2024-10-28 15:50:10 -0300

    X and Z model batteries prepared

--- src/ChargingPower.jl
+++ src/ChargingPower.jl
@@ -6,5 +6,7 @@ using Graphs
 export majorana_operators, SYK_hamiltonian
 
 include(joinpath(@__DIR__, "building_operators/majorana_operators.jl"))
+include(joinpath(@__DIR__, "building_operators/SYK_hamiltonian.jl"))
+include(joinpath(@__DIR__, "building_operators/battery_hamiltonians.jl"))
 
 end # module ChargingPower
---
+++ src/building_operators/battery_hamiltonians.jl
@@ -0,0 +1,27 @@
+# This function creates the Hamiltonian for the Z-model
+function Z_model_hamiltonian(majorana_operators)
+    n_majorana = length(majorana_operators)
+    hamiltonian_matrix = spzeros(2^(n_majorana/2), 2^(n_majorana/2))
+    for i in 1:Int(floor(n_majorana/2)) 
+        hamiltonian_matrix .+= -2 * im .* majorana_operators[2 * i - 1] * majorana_operators[2 * i] 
+    end
+    return hamiltonian_matrix
+end
+
+# This function creates the Hamiltonian for the X-model
+function X_model_hamiltonian(majorana_operators)
+    n_majorana = length(majorana_operators)
+    hamiltonian_matrix = spzeros(2^(n_majorana/2), 2^(n_majorana/2))
+    for k in 1: Int(floor(n_majorana/2)) 
+        if k == 1
+            hamiltonian_matrix .+= sqrt(2) .* majorana_operators[1]
+        else
+            provisional_matrix = majorana_operators[1]
+            for j in 2:(2 * k - 1) 
+                provisional_matrix *= majorana_operators[j]
+            end
+            hamiltonian_matrix .+= 2^(k - 1/2) * (-1)^(k + 1) .* provisional_matrix
+        end
+    end
+    return hamiltonian_matrix
+end
