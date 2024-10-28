594a00c0e871495f742f07a90bd32f02e1f63b05
Dario Rosa <dario.rosa85@gmail.com>
2024-10-28 16:01:08 -0300

    X_model_hamiltonian and Z_model_hamiltonian exported

--- src/ChargingPower.jl
+++ src/ChargingPower.jl
@@ -3,7 +3,7 @@ module ChargingPower
 using SparseArrays
 using Graphs
 
-export majorana_operators, SYK_hamiltonian
+export majorana_operators, SYK_hamiltonian, Z_model_hamiltonian, X_model_hamiltonian
 
 include(joinpath(@__DIR__, "building_operators/majorana_operators.jl"))
 include(joinpath(@__DIR__, "building_operators/SYK_hamiltonian.jl"))
