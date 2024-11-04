54491f838f9f74d4a97d15a120b619e7550078c1
Dario Rosa <dario.rosa85@gmail.com>
2024-11-04 19:47:38 -0300

    model working. ready to merge for the data collection

--- src/ChargingPower.jl
+++ src/ChargingPower.jl
@@ -2,8 +2,9 @@ module ChargingPower
 
 using SparseArrays
 using Graphs
+using KrylovKit
 
-export majorana_operators, SYK_hamiltonian, Z_model_hamiltonian, X_model_hamiltonian
+export majorana_operators, SYK_hamiltonian, Z_model_hamiltonian, X_model_hamiltonian, fake_X_model_hamiltonian
 
 include(joinpath(@__DIR__, "building_operators/majorana_operators.jl"))
 include(joinpath(@__DIR__, "building_operators/SYK_hamiltonian.jl"))
