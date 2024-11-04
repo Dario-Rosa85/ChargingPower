bdf8aa0aed364d0f03dc0bf61484a77c454bb417
Dario Rosa <dario.rosa85@gmail.com>
2024-11-04 16:29:51 -0300

    two optiont for SYK normalization factor created

--- src/building_operators/SYK_hamiltonian.jl
+++ src/building_operators/SYK_hamiltonian.jl
@@ -2,9 +2,13 @@
 function SYK_hamiltonian(majorana_operators, graph)
     n_majorana = length(majorana_operators)
     SYK_hamiltonian_matrix = spzeros(ComplexF64,2^floor(Int, n_majorana/2), 2^floor(Int, n_majorana/2))
+    if length(edges(graph)) == n_majorana * (n_majorana - 1) /2
+        normalization_factor = (im/2) * sqrt(1/(n_majorana))
+    else
+        normalization_factor = (im/2) * sqrt(n_majorana/(2 * length(edges(graph))))
+    end
     for edge in edges(graph) 
-        # SYK_hamiltonian_matrix .+= (im/2) * sqrt(n_majorana/(2 * length(edges(graph)))) * randn().* majorana_operators[src(edge)] * majorana_operators[dst(edge)]
-        SYK_hamiltonian_matrix .+= (im/2) * sqrt(1/(n_majorana)) * randn().* majorana_operators[src(edge)] * majorana_operators[dst(edge)]
+        SYK_hamiltonian_matrix .+= normalization_factor * randn().* majorana_operators[src(edge)] * majorana_operators[dst(edge)]
     end
     return SYK_hamiltonian_matrix
 end
