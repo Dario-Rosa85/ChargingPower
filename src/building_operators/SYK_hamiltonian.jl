e492d77d4c749b01b9f124256441d4ebc64e50b9
Dario Rosa <dario.rosa85@gmail.com>
2024-11-05 00:10:01 -0300

    twisted circle rewritten for better performance

--- src/building_operators/SYK_hamiltonian.jl
+++ src/building_operators/SYK_hamiltonian.jl
@@ -19,16 +19,16 @@ function twisted_circle_SYK_hamiltonian(majorana_operators, graph)
     SYK_hamiltonian_matrix = spzeros(ComplexF64,2^floor(Int, n_majorana/2), 2^floor(Int, n_majorana/2))
     normalization_factor = (im/2) * sqrt(n_majorana/(2 * length(edges(graph))))
     for edge in edges(graph)
-        if src(edge) + dst(edge) == n_majorana + 1
-            SYK_hamiltonian_matrix .+= normalization_factor * randn().* majorana_operators[src(edge)] * majorana_operators[dst(edge)]
+        if src(edge) + dst(edge) > n_majorana + 1
+            to_be_changed = min(src(edge), dst(edge))
+            to_remain = max(src(edge), dst(edge))
+            SYK_hamiltonian_matrix .+= normalization_factor * randn().* majorana_operators[n_majorana + 1 - to_be_changed] * majorana_operators[to_remain]
         elseif src(edge) + dst(edge) < n_majorana + 1
             to_be_changed = max(src(edge), dst(edge))
             to_remain = min(src(edge), dst(edge))
             SYK_hamiltonian_matrix .+= normalization_factor * randn().* majorana_operators[to_remain] * majorana_operators[n_majorana + 1 - to_be_changed]
         else
-            to_be_changed = min(src(edge), dst(edge))
-            to_remain = max(src(edge), dst(edge))
-            SYK_hamiltonian_matrix .+= normalization_factor * randn().* majorana_operators[n_majorana + 1 - to_be_changed] * majorana_operators[to_remain]
+            SYK_hamiltonian_matrix .+= normalization_factor * randn().* majorana_operators[src(edge)] * majorana_operators[dst(edge)]
         end 
     end
     return SYK_hamiltonian_matrix
